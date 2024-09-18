01f4e1aec78347460a4eb1bf1cc5897ec0546dbf
Angus2510 <124569463+Angus2510@users.noreply.github.com>
2024-09-18 14:10:02 +0200

    finished with hover effects

--- components/card.tsx
+++ components/card.tsx
@@ -5,13 +5,29 @@ const Card = () => {
   return (
     <div className="container bg-slate-800 w-80 rounded-xl  items-center flex flex-col shadow-lg ">
       <div className="flex items-center ">
-        <Image
-          src="/images/image-equilibrium.jpg"
-          alt="image"
-          width={300}
-          height={300}
-          className=" flex items-center rounded-2xl p-2 pt-4 hover:bg-cyan-400"
-        />
+        <div className="relative max-w-xs overflow-hidden bg-cover bg-no-repeat">
+          <Image
+            src="/images/image-equilibrium.jpg"
+            alt="image"
+            width={300}
+            height={300}
+            className=" flex items-center rounded-2xl  mt-4 hover:bg-cyan-400 hover:bg-cover"
+          />
+          <div className=" flex items-center mt-4 justify-center rounded-b-2xl rounded-2xl absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-cyan-400 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-50">
+            <span>
+              <svg width="48" height="48" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fill-rule="evenodd">
+                  <path d="M0 0h48v48H0z" />
+                  <path
+                    d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15Zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10Zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6Z"
+                    fill="#FFF"
+                    fill-rule="nonzero"
+                  />
+                </g>
+              </svg>
+            </span>
+          </div>
+        </div>
       </div>
       <div className="ml-6 mt-4">
         <h1 className="text-white mb-4 font-semibold  hover:text-cyan-400 ">
